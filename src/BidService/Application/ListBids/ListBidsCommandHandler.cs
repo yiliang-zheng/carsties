17b1a74d7bbbd9714ba918d2750d793022be7652
Yiliang Zheng <yiliang.zheng0729@gmail.com>
2024-06-16 12:29:38 +1000

    update dockerfile

--- src/BidService/Application/ListBids/ListBidsCommandHandler.cs
+++ src/BidService/Application/ListBids/ListBidsCommandHandler.cs
@@ -1,20 +1,48 @@
 ﻿using AutoMapper;
+using Domain.Bid;
 using Domain.Bid.Repository;
 using Domain.Bid.Specification;
 using FluentResults;
+using FluentResults.Extensions;
 using MediatR;
 
 namespace Application.ListBids;
 
 public class ListBidsCommandHandler(IBidRepository bidRepository, IMapper mapper) : IRequestHandler<ListBidsCommand, Result<List<BidDto>>>
 {
-    
+    /// <summary>
+    /// 1. create spec
+    /// 2. get bids by spec
+    /// 3. map to bid dto
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<Result<List<BidDto>>> Handle(ListBidsCommand request, CancellationToken cancellationToken)
     {
-        var spec = new BidsByAuctionSpec(request.AuctionId);
-        var entities = await bidRepository.ListAsync(spec, cancellationToken);
-        if (entities is null) return Result.Fail<List<BidDto>>("bids not found");
+        var result = await CreateSpec(request)
+            .Bind(spec => GetBids(spec, cancellationToken))
+            .Bind(MapDto);
 
+        return result;
+    }
+
+    private Result<BidsByAuctionSpec> CreateSpec(ListBidsCommand request)
+    {
+        var spec = new BidsByAuctionSpec(request.AuctionId);
+        return Result.Ok(spec);
+    }
+
+    private async Task<Result<List<Bid>>> GetBids(BidsByAuctionSpec spec, CancellationToken cancellationToken)
+    {
+        var entities = await bidRepository.ListAsync(spec, cancellationToken);
+        if (entities is null) return Result.Fail<List<Bid>>("bids not found");
+
+        return Result.Ok(entities);
+    }
+
+    private Result<List<BidDto>> MapDto(List<Bid> entities)
+    {
         var result = entities.Select(mapper.Map<BidDto>).ToList();
         return result;
     }
